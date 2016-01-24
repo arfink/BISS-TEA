ac1a738aa691475df68e59793878a95949d05ef0
Jared Skinner <jaredds66@gmail.com>
2016-01-23 22:44:24 -0600

    added url handeling, and template pages for basic pages.  Updated HTMLTemplate to use a bootstrap style

--- index.php
+++ index.php
@@ -1,6 +1,66 @@
 <?php
 
-	// Show all information, defaults to INFO_ALL
-	phpinfo();
+//make sure the url entered is valid
+function validate_url($url_html, $url_php)
+{
+	if (!file_exists($url_html) && !file_exists($url_php))
+		return FALSE;
+
+	return TRUE;
+}
+
+
+//parse url and serve up appropriate files
+function parse($url)
+{
+	//parse $url.  Essentially we are going to take what is given and remove any extension
+	$url_array = explode(".", $url);
+	
+	//defind paths
+	$url_html = "templates/".$url_array[0].".html";
+	$url_php = "php/pages/".$url_array[0].".php";
+
+
+
+	//validate url
+	if (!validate_url($url_html, $url_php))
+	{
+		$url_html = "templates/home.html";
+		$url_php = "php/pages/home.php";
+		//echo $url_html;
+	}
+	
+
+
+	//load generic template file
+	$html = file_get_contents("templates/HTMLTemplate");
+
+	//load body file
+	$body = file_get_contents($url_html);
+
+	//clear out permission variable
+
+	//execute page specific php, if it exists
+	file_exists($url_php);	
+	include $url_php;
+
+
+	//check permission variable against user account.
+	//If permission variable not set, die loudly
+
+
+
+	//push body content into $html
+	$html = str_replace("==body==", $body, $html);	
+
+	//echo out resulting webpage
+	echo $html;
+}
+
+//check if user is logged in, if not return to the login screen and exit
+
+
+
+parse($_SERVER["REQUEST_URI"]);
 
 ?>
