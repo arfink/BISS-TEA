3bcc5beb1cbd450eff1c0bb6cb125068fe0b8bfa
Jared Skinner <jaredds66@gmail.com>
2016-01-25 21:41:18 -0600

    added signup page, css for login

---
+++ php/actions/mysql_functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function connect_to_mysql()
+{
+	$servername = "localhost";
+	$username = "php";
+	$password = "password";
+	$database = "biss";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $database);
+
+	// Check connection
+	if ($conn->connect_error) {
+		//die("Connection failed: " . $conn->connect_error);
+		return FALSE;
+	} 
+
+	echo "connected successfully";
+	return $conn;
+}
+
+
+?>
